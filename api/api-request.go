263432ebd603d376d3fa3e63db8b1a4e3b3715ee
Isaac <46573506+wuve@users.noreply.github.com>
2021-04-09 02:46:08 +0100

    post request

--- api/api-request.go
+++ api/api-request.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 )
 
 func main() {
+	//Get request
 	response, err := http.Get("https://api.coingecko.com/api/v3/simple/price?ids=dogecoin&vs_currencies=gbp")
 
 	if err != nil {
@@ -14,5 +17,17 @@ func main() {
 	}
 
 	content, _ := ioutil.ReadAll(response.Body)
+	fmt.Println(string(content))
+
+	//Post request
+	Data := map[string]string{"username": "wuve", "colour": "pink"}
+	json, _ := json.Marshal(Data)                                                                    //Marshal map into json
+	response, err = http.Post("https://httpbin.org/post", "application/json", bytes.NewBuffer(json)) //Create request(post endpoint)/header(content type)/buffer json to byte
+
+	if err != nil {
+		fmt.Printf("error occured, %s", err)
+	}
+
+	content, _ = ioutil.ReadAll(response.Body)
 	fmt.Println(string(content))
 }
